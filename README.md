24ec9648df7092625a8057715501e152cb2ee1df
Jessica Card <jessicard@mac.com>
2022-03-10 14:46:19 -0500

    Add README and clean up

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Site 1 Example
+# Contribute to Hack Club
+
+Hack Club is a nonprofit network of high school coding clubs and makers around the world. The Hack Club community has over 250 open source repositories, many of which are being actively developed every day. What makes these repositories open source is that they are editable by the public, which encourages you, the Hack Club community, to come together and work on code socially. Contributions can be made in many ways, like tracking bugs and features by opening a GitHub issue, editing code by creating a GitHub pull request, and reviewing pull requests made by others. We welcome and encourage your contributions, and we look forward to seeing the things you create!
+
+## Contributing
+
+Contributions are encouraged and welcome! Feel free to submit a pull request with code changes, or open issues for suggestions or bug reports.
+
+Development chatter happens in the [#hackclub-site-dev](https://app.slack.com/client/T0266FRGM/C036BTDGP43) channel in the [Hack Club Slack](https://hackclub.com/slack/).
+
+## Running Locally
+
+1. Clone this repository
+   - `git clone https://github.com/hackclub/contribute.git && cd contribute`
+1. Start server
+   - `python -m SimpleHTTPServer`
+1. View your server
+   - `open http://localhost:8000/`
--- script.js
+++ script.js
@@ -1,36 +1,26 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-  // let repos = [
-  //   "hackclub/scrapbook",
-  //   "hackclub/scrappy",
-  //   "hackclub/gamelab",
-  //   "hackclub/workshops",
-  //   "jessicard/hacker-challenge",
-  // ]
-  // let url = "https://api.github.com/repos/jessicard/hacker-challenge/issues\?labels\=good%20first%20issue";
-  // let url = "https://api.github.com/repos/" + repo + "/issues";
-
   let url = "https://api.github.com/users/hackclub/repos\?sort=pushed"
 
-  // fetch(url)
-  //   .then(function(response) {
-  //     return response.json();
-  //   }).then(function(repos) {
-  //     repos.forEach((repo) => {
-  //       let reposListEl = document.querySelector(".repos");
-  //       let repoEl = document.querySelector(".example-repo");
-  //
-  //       if (repo['has_issues']) {
-  //         console.log(repo['name']);
-  //         console.log(repo['open_issues_count']);
-  //         console.log(repo['html_url']);
-  //         console.log(repo['language']);
-  //         console.log(repo['description']);
-  //         console.log("Last pushed at: " + repo['pushed_at']);
-  //         console.log('---------------------------');
-  //       };
-  //     })
-  //   }).catch(function() {
-  //     console.log("Booo");
-  //   }
-  // );
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    }).then(function(repos) {
+      repos.forEach((repo) => {
+        let reposListEl = document.querySelector(".repos");
+        let repoEl = document.querySelector(".example-repo");
+
+        if (repo['has_issues']) {
+          console.log(repo['name']);
+          console.log(repo['open_issues_count']);
+          console.log(repo['html_url']);
+          console.log(repo['language']);
+          console.log(repo['description']);
+          console.log("Last pushed at: " + repo['pushed_at']);
+          console.log('---------------------------');
+        };
+      })
+    }).catch(function() {
+      console.log("Fetching " + url + "failed");
+    }
+  );
 });
