2778e217d4653f89bb5de9f209a9169d3c2ea1ec
Jessica Card <jessicard@mac.com>
2022-03-14 13:29:38 -0400

    Make repo list use API

--- script.js
+++ script.js
@@ -5,22 +5,43 @@ window.addEventListener("DOMContentLoaded", (event) => {
     .then(function(response) {
       return response.json();
     }).then(function(repos) {
-      repos.forEach((repo) => {
-        let reposListEl = document.querySelector(".repos");
-        let repoEl = document.querySelector(".example-repo");
+      if (repos.length > 0) {
+        repos.forEach((repo) => {
+          let openIssuesCount = repo["open_issues_count"];
+          if (openIssuesCount > 0) {
+            let reposListEl = document.querySelector("[data-tag='repos'] ul");
+            let exampleEl = document.querySelector("[data-tag='example-repo']");
 
-        if (repo['has_issues']) {
-          console.log(repo['name']);
-          console.log(repo['open_issues_count']);
-          console.log(repo['html_url']);
-          console.log(repo['language']);
-          console.log(repo['description']);
-          console.log("Last pushed at: " + repo['pushed_at']);
-          console.log('---------------------------');
-        };
-      })
-    }).catch(function() {
-      console.log("Fetching " + url + "failed");
+            let repoEl = exampleEl.cloneNode(true);
+            repoEl.classList.remove("hidden");
+
+            repoEl.querySelector("[data-tag='repo-link']")
+              .href = repo["html_url"];
+            repoEl.querySelector("[data-tag='name']")
+              .innerText = repo["name"];
+            repoEl.querySelector("[data-tag='language']")
+              .innerText = repo["language"];
+            repoEl.querySelector("[data-tag='issues-count']")
+              .innerText = openIssuesCount;
+            repoEl.querySelector("[data-tag='description']")
+              .innerText = repo["description"];
+            repoEl.querySelector("[data-tag='last-push']")
+              .innerText = repo["pushed_at"];
+
+            reposListEl.appendChild(repoEl);
+          }
+        })
+      } else {
+        showEmptyMessage();
+      }
+    }).catch(function(err) {
+      console.log("Fetching " + url + " failed");
+      console.log("Error: " + err);
     }
   );
+
+  function showEmptyMessage() {
+    let noReposEl = document.querySelector("[data-tag='no-repos']");
+    noReposEl.classList.remove("hidden");
+  }
 });
